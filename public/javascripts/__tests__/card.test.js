e33e6ca977acc08a19339ac9959a043dd14a7739
FelixFamiliaRodriguez <frodriguez@familiarodriguez.org>
2021-01-04 20:02:06 -0500

    Completed card unit tests

--- public/javascripts/__tests__/card.test.js
+++ public/javascripts/__tests__/card.test.js
@@ -1,6 +1,11 @@
 const index = require('../card.js');
+const $ = require('jquery');
 
 test('Card Constructor', () => {
+
+    document.body.innerHTML = 
+        '<div></div>';
+
     let card = new index.Card('images/test.png', 'aceofspades', 'spades', 1);
     expect(card.isFaceDown()).toBe(true);
     expect(card.suit).toBe('spades');
@@ -8,4 +13,45 @@ test('Card Constructor', () => {
     expect(card.color).toBe('black');
     expect(card.frontImage).toBe('images/test.png');
     expect(card.backImage).toBe('images/cardback.png');
+
+    const spyHandleClick = jest.spyOn(card, 'handleClick');
+
+    const spyhandleDragStart = jest.spyOn(card, 'handleDragStart');
+
+    const event = {
+        type: "click"
+    }
+
+    card.handleEvent(event);
+
+    expect(spyHandleClick).toBeCalled();
+
+    const event2 = {
+        type: "dragstart"
+    }
+
+    card.handleEvent(event2);
+
+    expect(spyhandleDragStart).toBeCalled();
+
+    const spySetTop = jest.spyOn(card, 'setTop');
+
+    card.setTop(13);
+
+    expect(spySetTop).toBeCalled();
+
+    const spyZIndex = jest.spyOn(card, 'setZIndex');
+
+    card.setZIndex(1000);
+
+    expect(spyZIndex).toBeCalled();
+
+    card.flipCard();
+
+    expect(card.isFaceDown()).toBe(false);
+
+    card.flipCard();
+
+    expect(card.isFaceDown()).toBe(true);
+    
 });
