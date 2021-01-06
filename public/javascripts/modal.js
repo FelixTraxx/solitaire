c253f71d7cd134dca5c014ca627679574431b0f6
FelixFamiliaRodriguez <frodriguez@familiarodriguez.org>
2021-01-06 07:52:12 -0500

    Added modal dialog box announcing that you won

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -3,6 +3,7 @@ class Modal {
 
     constructor() {
         this.element = document.getElementById("modal");
+        this.element.style.zIndex = 1000;
         this.closeButton = document.getElementById("modal-close");
         this.closeButton.addEventListener("click", this);
     }
